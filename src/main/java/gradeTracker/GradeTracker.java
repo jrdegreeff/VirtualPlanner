5f48404dc0902c1afa3293e108e984aded6330d8
jere99 <jrdegreeff@gmail.com>
2018-05-06 01:31:17 -0400

    Main class and gitignore

---
+++ src/main/java/gradeTracker/GradeTracker.java
@@ -0,0 +1,17 @@
+package gradeTracker;
+
+/**
+ * Main class for the GradeTracker project.
+ * 
+ * @author AnnaDai
+ * @author JeremiahDeGreeff
+ * @author LeoDong
+ * @author KevinGao
+ */
+public class GradeTracker {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
