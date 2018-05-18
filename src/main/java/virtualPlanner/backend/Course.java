fd04a83d71bb56178a83b6b8e9e6e9f15c684db4
LeoAppleseed <leodong0315@163.com>
2018-05-18 08:12:39 -0400

    Course class completed
    with changeAssnDate and changeDueDate

--- src/main/java/virtualPlanner/backend/Course.java
+++ src/main/java/virtualPlanner/backend/Course.java
@@ -9,25 +9,26 @@ import virtualPlanner.util.Date;
 /**
  * The Course class maps dates to a TreeSet of Assignment objects.
  * @author aldai
+ * @author Leo (changeAssignedDate and changeDueDate)
  * 
  */
 public class Course {
-	
+
 	/**
 	 * course name and teacher
 	 */
 	private String name, teacher;
-	
+
 	/**
 	 * maps Dates to a set of all Assignments assigned that day
 	 */
-	private Map<Date, TreeSet<Assignment>> assigned;
-	
+	private Map<Date, TreeSet<Assignment>> assnDateMap;
+
 	/**
 	 * maps Dates to a set of all Assignments due that day
 	 */
-	private Map<Date, TreeSet<Assignment>> due;
-	
+	private Map<Date, TreeSet<Assignment>> dueDateMap;
+
 	/**
 	 * Constructor for Course class. assigned and due maps are set to empty HashMaps.
 	 * @param name
@@ -36,76 +37,114 @@ public class Course {
 	public Course(String name, String teacher) {
 		this.name = name;
 		this.teacher = teacher;
-		assigned = new HashMap<Date, TreeSet<Assignment>>();
-		due = new HashMap<Date, TreeSet<Assignment>>();
+		assnDateMap = new HashMap<Date, TreeSet<Assignment>>();
+		dueDateMap = new HashMap<Date, TreeSet<Assignment>>();
 	}
-	
+
 	/**
 	 * @return course name
 	 */
 	public String getCourseName() {
 		return name;
 	}
-	
+
 	/**
 	 * @return teacher name
 	 */
 	public String getTeacher() {
 		return teacher;
 	}
-	
+
 	/**
 	 * @param dateDue
 	 * @return TreeSet of Assignments due on a given date.
 	 */
 	public TreeSet<Assignment> getDue(Date dateDue) {
-		return due.get(dateDue);
+		return dueDateMap.get(dateDue);
 	}
-	
+
 	/** 
 	 * @param dateAssigned
 	 * @return TreeSet of Assignments assigned on a given date.
 	 */
 	public TreeSet<Assignment> getAssigned(Date dateAssigned) {
-		return assigned.get(dateAssigned);
+		return assnDateMap.get(dateAssigned);
 	}
-	
+
 	/**
 	 * addAssignment adds the given Assignment object to the Assigned HashMap
 	 * and the Due HashMap by respective dates.
 	 * @param hw
 	 */
 	public void addAssignment(Assignment hw) {
-		
+
 		Date dateDue = hw.getDue();
 		Date dateAssigned = hw.getAssignedDate();
-		
-		if (!due.containsKey(dateDue)) {
+
+		if (!dueDateMap.containsKey(dateDue)) {
 			TreeSet<Assignment> homework = new TreeSet<Assignment>();
 			homework.add(hw);
-			due.put(dateDue, homework);
+			dueDateMap.put(dateDue, homework);
 		}
 		else {
-			due.get(dateDue).add(hw);
+			dueDateMap.get(dateDue).add(hw);
 		}
-		
-		if (!assigned.containsKey(dateAssigned)) {
+
+		if (!assnDateMap.containsKey(dateAssigned)) {
 			TreeSet<Assignment> homework = new TreeSet<Assignment>();
 			homework.add(hw);
-			assigned.put(dateAssigned, homework);
+			assnDateMap.put(dateAssigned, homework);
 		}
 		else {
-			assigned.get(dateAssigned).add(hw);
+			assnDateMap.get(dateAssigned).add(hw);
 		}
 	}
+
+	/**
+	 * Change the assigned date for a particular assignment. 
+	 * @param assn assignment
+	 * @param newAssnDate new assigned date
+	 */
+	public void changeAssignedDate(Assignment assn, Date newAssnDate) {
+		// update assigned date in assignment
+		Date oldAssnDate = assn.changeAssignedDate(newAssnDate);
+
+		// ArrayList of assignment on old assignment date
+		TreeSet<Assignment> oldAssignments = assnDateMap.get(oldAssnDate); // old assignments set for old date
+		oldAssignments.remove(assn);
+		TreeSet<Assignment> newAssignments = assnDateMap.get(newAssnDate); // new assignments set for new date
+		newAssignments.add(assn);
+		
+		assnDateMap.put(newAssnDate, newAssignments);
+	}
 	
+	
+	/**
+	 * Change the due date for a particular assignment. 
+	 * @param assn assignment
+	 * @param newDueDate new due date
+	 */
+	public void changeDueDate(Assignment assn, Date newDueDate) {
+		// update assigned date in assignment
+		Date oldDueDate = assn.changeDueDate(newDueDate);
+
+		// ArrayList of assignment on old assignment date
+		TreeSet<Assignment> oldAssignments = assnDateMap.get(oldDueDate); // old assignments set for old date
+		oldAssignments.remove(assn);
+		TreeSet<Assignment> newAssignments = assnDateMap.get(newDueDate); // new assignments set for new date
+		newAssignments.add(assn);
+		
+		assnDateMap.put(newDueDate, newAssignments);
+	}
+
+
 	/**
 	 * @return true if two courses have the same course name and teacher.
 	 */
 	public boolean equals(Course other) {
 		return name.equals(other.getCourseName()) && teacher.equals(other.getTeacher());
 	}
-	
+
 	/**
 	 * @return course name taught by teacher
 	 */
