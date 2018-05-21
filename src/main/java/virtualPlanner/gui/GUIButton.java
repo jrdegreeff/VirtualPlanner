fe4b14b06464b34220f194cc74979c32b71ae922
KevinGao6 <32113541+KevinGao6@users.noreply.github.com>
2018-05-21 08:17:09 -0400

    Text Wrap features

--- src/main/java/virtualPlanner/gui/GUIButton.java
+++ src/main/java/virtualPlanner/gui/GUIButton.java
@@ -2,10 +2,23 @@ package virtualPlanner.gui;
 
 import javax.swing.JButton;
 
+@SuppressWarnings("serial")
 public class GUIButton extends JButton
 {
 	public GUIButton(String name)
 	{
-		super(name);
+		super();
+		String textWrap = "Text\nWrap\nV1";
+		this.setText("<html>" + textWrap.replaceAll("\\n", "<br>") + "</html>");
+	}
+	
+	/**
+	 * Enables multi-line text
+	 * PRECONDITION: New lines are denotated by a "\n"
+	 * @param text
+	 */
+	public void setMultiLineText(String text)
+	{
+		this.setText("<html>" + text.replaceAll("\\n", "<br>") + "</html>");
 	}
 }
