05496b54a866563917b2791973f586c618491b55
jere99 <jrdegreeff@gmail.com>
2018-05-21 09:23:36 -0400

    fix null pointer

--- src/main/java/virtualPlanner/gui/GUIController.java
+++ src/main/java/virtualPlanner/gui/GUIController.java
@@ -37,10 +37,11 @@ public class GUIController {
 	 * Retrieves the id of the {@code Course} from the user's schedule in the specified {@code Block}.
 	 * 
 	 * @param block The {@code Block} to find the {@code Course} for.
-	 * @return The id of the {@code Course} associated with the specified {@code Block}, or {@code null} if the user has no {@code Course} for that {@code Block}.
+	 * @return The id of the {@code Course} associated with the specified {@code Block}, or a value of -1 if the user has no {@code Course} for that {@code Block}.
 	 */
 	public int getCourseID(Block block) {
-		return user.getCourse(block).getID();
+		Course course = user.getCourse(block);
+		return course == null ? -1 : course.getID();
 	}
 	
 	/**
