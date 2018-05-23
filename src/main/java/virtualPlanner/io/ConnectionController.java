7c3f0b813795730fbaf29bf4d0ef0de484d52658
jere99 <jrdegreeff@gmail.com>
2018-05-23 10:58:40 -0400

    beginning of io

---
+++ src/main/java/virtualPlanner/io/ConnectionController.java
@@ -0,0 +1,16 @@
+package virtualPlanner.io;
+
+import java.sql.Connection;
+
+public class ConnectionController {
+	
+	/**
+	 * Connects to the database.
+	 * 
+	 * @return The {@code Connection}.
+	 */
+	public static Connection connect() {
+		return null;
+	}
+	
+}
