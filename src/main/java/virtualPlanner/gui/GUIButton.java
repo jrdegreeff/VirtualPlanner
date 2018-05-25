2b7f2693af57ba3e0da0b77ffcf85a79270aac5a
KevinGao6 <32113541+KevinGao6@users.noreply.github.com>
2018-05-25 01:51:42 -0400

    Quick Update

--- src/main/java/virtualPlanner/gui/GUIButton.java
+++ src/main/java/virtualPlanner/gui/GUIButton.java
@@ -227,7 +227,7 @@ public class GUIButton extends JButton implements ActionListener {
 		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		monthBox = new JComboBox<String>(months);
 		monthBox.setBackground(Color.WHITE);
-		
+		//TODO: Getter method for date
 		String[] days = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
 		dayBox = new JComboBox<String>(days);
 		dayBox.setBackground(Color.WHITE);
