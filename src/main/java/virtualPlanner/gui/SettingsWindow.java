52aa288dd6d75f1ba77a6c18e9724ade8e1faf6f
KevinGao6 <32113541+KevinGao6@users.noreply.github.com>
2018-05-28 18:40:09 -0400

    Quick Check

--- src/main/java/virtualPlanner/gui/SettingsWindow.java
+++ src/main/java/virtualPlanner/gui/SettingsWindow.java
@@ -71,6 +71,7 @@ public class SettingsWindow {
 					Preferences.addColor(b.getCourse().getID(), b.getBackground());
 				}
 				frame.dispose();
+				parent.update();
 				parent.closeSettings();
 			}
 		});
